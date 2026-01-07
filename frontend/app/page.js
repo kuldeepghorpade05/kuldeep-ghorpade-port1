358626d59403f23df2f35601d77d7151cde8b302
kuldeep ghorpade <kuldeepghorpade05@gmail.com>
2026-01-07 15:49:25 +0530

    refactor: clean up unused code in page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -9,20 +9,6 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import ContributionGraph from "./components/homepage/contribution-graph";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
 export default async function Home() {
   const blogs = personalBlogData;
 
